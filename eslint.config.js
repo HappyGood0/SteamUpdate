d0b113a771f87002d89eaefc5d35fb70bf069a1a
Rubenso <Rubens.tueux@cy-tech.fr>
2026-02-08 15:19:29 +0100

    feat: Ajout du fichier de config eslint pour analyser le js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    files: ["web/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
+    }
+  }
+];
